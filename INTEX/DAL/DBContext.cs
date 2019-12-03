ef806f1bdecf3e3635e6464f2f364c63d2b8d473
SunnyDcitruspunch <wiwisun18@gmail.com>
2019-12-03 10:55:06 -0700

    Add about page

---
+++ INTEX/DAL/DBContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace INTEX.DAL
+{
+    public class DBContext : DbContext
+    {
+        public DBContext() : base("DBContext")
+        {
+
+        }
+    }
+}
