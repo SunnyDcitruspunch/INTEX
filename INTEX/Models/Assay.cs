eaf5b5d4715f5701547c6b64c0fdc6af8ff274f4
DMOrrock <50670252+DMOrrock@users.noreply.github.com>
2019-12-03 17:56:48 -0700

    Catalog stuff

--- INTEX/Models/Assay.cs
+++ INTEX/Models/Assay.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,5 +10,25 @@ namespace INTEX.Models
 
     public class Assay
     {
+        [Display(Name = "Assay Number")]
+        public int AssayID { get; set; }
+
+        [Display(Name = "Type of Assay")]
+        public string TypeCode { get; set; }
+
+        [Display(Name = "Summary of assay")]
+        public string SummaryInfo { get; set; }
+
+        [Display(Name = "Information about procedure")]
+        public string DetailedInfo { get; set; }
+
+        [Display(Name = "Procedure Details")]
+        public string Procedure { get; set; }
+
+        [Display(Name = "Literary Reference")]
+        public string LiteratureReference { get; set; }
+        
+        [Display(Name="Estimated days to complete assay")]
+        public int EstimaEstimateDaysToComplete { get; set; }
     }
 }
